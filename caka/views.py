1bc5f037da49ed4079787dad725302f92d7c5325
nabhanyuzqi1 <nabhanyuzqi1@gmail.com>
2022-11-13 17:24:55 +0700

    add: add features and minor changes

--- caka/views.py
+++ caka/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import redirect, render
+from caka.models import Categories, Course
 
 
 def BASE(request):
     return render(request, 'base.html')
 
+
 def HOME(request):
-    return render(request, 'Main/home.html')
+    category = Categories.objects.all().order_by('id')[0:5]
+    course = Course.objects.filter(status='PUBLISH').order_by('-id')
+    print(course)
+    context = {
+        'category': category,
+        'course': course,
+    }
+    return render(request, 'Main/home.html', context)
+
 
 def SINGLE_COURSE(request):
     return render(request, 'Main/single_course.html')
 
+
 def LIST_COURSE(request):
-    return render(request, 'Main/list_course.html')
+    category = Categories.get_all_category(Categories)
+    context = {
+        'category': category,
+    }
+
+    return render(request, 'Main/list_course.html', context)
+
 
 def CONTACT_US(request):
     return render(request, 'Main/contact_us.html')
+
 
 def ABOUT_US(request):
     return render(request, 'Main/about_us.html')
--- config/urls.py
+++ config/urls.py
@@ -1,25 +1,28 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from caka import views
 from caka.auth import userlogin
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+                  path('admin/', admin.site.urls),
 
-    path('base', views.BASE, name='base'),
-    path('', views.HOME, name='home'),
-    path('contact', views.CONTACT_US, name='contact_us'),
-    path('about', views.ABOUT_US, name='about_us'),
-    path('course/single', views.SINGLE_COURSE, name='single_course'),
-    path('course/list', views.LIST_COURSE, name='list_course'),
+                  path('base', views.BASE, name='base'),
+                  path('', views.HOME, name='home'),
 
-    path('accounts/', include('django.contrib.auth.urls')),
+                  path('contact', views.CONTACT_US, name='contact_us'),
+                  path('about', views.ABOUT_US, name='about_us'),
+                  path('course/single', views.SINGLE_COURSE, name='single_course'),
+                  path('courses', views.LIST_COURSE, name='list_course'),
 
-    path('doLogin', userlogin.DO_LOGIN, name='doLogin'),
-    path('accounts/register', userlogin.REGISTER, name='register'),
-    path('accounts/profile', userlogin.PROFILE, name='profile'),
-    path('accounts/profile/update', userlogin.PROFILE_UPDATE, name='profile_update'),
+                  path('accounts/', include('django.contrib.auth.urls')),
 
-    # custom pages
-    path('customLogin', views.CUSTOM_LOGIN, name='customLogin'),
-]
+                  path('doLogin', userlogin.DO_LOGIN, name='doLogin'),
+                  path('accounts/register', userlogin.REGISTER, name='register'),
+                  path('accounts/profile', userlogin.PROFILE, name='profile'),
+                  path('accounts/profile/update', userlogin.PROFILE_UPDATE, name='profile_update'),
+
+                  # custom pages
+                  path('customLogin', views.CUSTOM_LOGIN, name='customLogin'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
