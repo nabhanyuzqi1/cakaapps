af9ba5717ab9e773b1afff5d01faa0fa4ceecf02
nabhanyuzqi1 <nabhanyuzqi1@gmail.com>
2022-11-16 00:19:05 +0700

    Automatic configuration with django-heroku

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,8 @@
 import os
+import django_heroku
 from pathlib import Path
 
+django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -108,10 +110,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, "static")
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -127,3 +129,4 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nabhan.caka@gmail.com'
 EMAIL_HOST_PASSWORD = 'dbuozseoioliwqzy'
+
