0a65feb7c6180912f71bd85d666f93f1b508e53f
nabhanyuzqi1 <nabhanyuzqi1@gmail.com>
2022-11-15 23:29:19 +0700

    fix: failed to deploy on collectstatic

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-#v-_e@q4w1%eq*m$jnp@0_wa3i8kvtasjvt^81rodfa54^q((b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cakrawalaakademi.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -108,10 +108,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
