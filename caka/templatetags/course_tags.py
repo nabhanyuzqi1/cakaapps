a09756c18cd716d562a839319e8d4469170147a1
nabhanyuzqi1 <nabhanyuzqi1@gmail.com>
2022-11-15 21:11:28 +0700

    fix: some minor changes on course list page

--- caka/templatetags/course_tags.py
+++ caka/templatetags/course_tags.py
@@ -5,10 +5,7 @@ register = template.Library()
 
 @register.simple_tag
 def discount_calculation(price,discount):
-    if price is None or price is 0:
-        price = 'FREE'
-        return price
-    elif discount is None or discount is 0:
+    if discount is None or discount is 0:
         return price
     sellprice = price
     sellprice = price - (price * discount/100)
