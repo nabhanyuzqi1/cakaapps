2f16983b2fcdc783b6f80877d99f9dddbf012b72
nabhanyuzqi1 <nabhanyuzqi1@gmail.com>
2022-11-16 03:03:38 +0700

    add: automatic slug generator fo course

--- caka/models.py
+++ caka/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
 
 # Create your models here.
 class Categories(models.Model):
@@ -60,3 +61,22 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Course.objects.filter(slug=slug).order_by('-id')
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" % (slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_receiver, Course)
