96945caf83c8a9995e954e7ded73da7ccaeede93
nabhanyuzqi1 <nabhanyuzqi1@gmail.com>
2022-11-20 06:59:23 +0700

    fix: setting

--- config/settings.py
+++ config/settings.py
@@ -109,11 +109,13 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
-# Add these new lines
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -132,4 +134,4 @@ EMAIL_HOST_PASSWORD = 'dbuozseoioliwqzy'
 CSRF_TRUSTED_ORIGINS = ['https://cakrawalaakademi.herokuapp.com', 'https://cakrawalaakademi.com']
 
 import django_heroku
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
